9de8b8b5a652300f050c769ef79e7f19e10515a7
大畠千惠子 <>
2024-10-02 13:53:59 +0900

    「選択科目での検索機能を追加」の修正を行いました。

--- app/Http/Controllers/Authenticated/Users/UsersController.php
+++ app/Http/Controllers/Authenticated/Users/UsersController.php
@@ -26,9 +26,10 @@ class UsersController extends Controller
         $subjects = $request->subject; // 追加された部分
         // requestの中のsubjectにname属性から引っ張ってくる
 
-
         $userFactory = new SearchResultFactories();
         $users = $userFactory->initializeUsers($keyword, $category, $updown, $gender, $role, $subjects);
+
+
 
         $subjects = Subjects::all(); // 選択科目の一覧を取得してビューに渡す処理
         // 絞った検索条件だけだと今の科目のみが選ばれてしまうので、allで上書きして他の科目が追加されても引き抜けるように記述
@@ -48,30 +49,5 @@ class UsersController extends Controller
         $user = User::findOrFail($request->user_id);
         $user->subjects()->sync($request->subjects);
         return redirect()->route('user.profile', ['id' => $request->user_id]);
-    }
-
-    public function searchUsers(Request $request)
-    // ユーザー検索　科目は一つでも当てはまれば表示されるようにする！
-    {
-        if ($request->input('category') === 'name') {
-            $users = User::where('over_name', 'like', '%' . $request->input('keyword') . '%')
-                ->orWhere('under_name', 'like', '%' . $request->input('keyword') . '%')
-                ->get();
-        } elseif ($request->input('category') === 'id') {
-            $users = User::where('id', $request->input('keyword'))->get();
-        }
-
-        if ($request->input('subjects')) {
-            $selectedSubjects = $request->input('subjects'); // 選択された科目IDを取得
-
-            // いずれかの科目に一致するユーザーを絞り込む
-            $users->whereHas('subjects', function ($q) use ($selectedSubjects) {
-                $q->whereIn('id', $selectedSubjects);
-            });
-        }
-
-        $users = $users->get();
-
-        return view('users.index', compact('users'));
     }
 }
--- app/Searchs/SelectNameDetails.php
+++ app/Searchs/SelectNameDetails.php
@@ -1,38 +1,44 @@
 <?php
+
 namespace App\Searchs;
 
 use App\Models\Users\User;
 
-class SelectNameDetails implements DisplayUsers{
+class SelectNameDetails implements DisplayUsers
+{
 
   // 改修課題：選択科目の検索機能
-  public function resultUsers($keyword, $category, $updown, $gender, $role, $subjects){
-    if(is_null($gender)){
+  public function resultUsers($keyword, $category, $updown, $gender, $role, $subjects)
+  {
+    if (is_null($gender)) {
       $gender = ['1', '2', '3'];
-    }else{
+    } else {
       $gender = array($gender);
     }
-    if(is_null($role)){
+    if (is_null($role)) {
       $role = ['1', '2', '3', '4'];
-    }else{
+    } else {
       $role = array($role);
     }
     $users = User::with('subjects')
-    ->where(function($q) use ($keyword){
-      $q->Where('over_name', 'like', '%'.$keyword.'%')
-      ->orWhere('under_name', 'like', '%'.$keyword.'%')
-      ->orWhere('over_name_kana', 'like', '%'.$keyword.'%')
-      ->orWhere('under_name_kana', 'like', '%'.$keyword.'%');
-    })
-    ->where(function($q) use ($role, $gender){
-      $q->whereIn('sex', $gender)
-      ->whereIn('role', $role);
-    })
-    ->whereHas('subjects', function($q) use ($subjects){
-      $q->where('subjects.id', $subjects);
-    })
-    ->orderBy('over_name_kana', $updown)->get();
+      ->where(function ($q) use ($keyword) {
+        $q->Where('over_name', 'like', '%' . $keyword . '%')
+          ->orWhere('under_name', 'like', '%' . $keyword . '%')
+          ->orWhere('over_name_kana', 'like', '%' . $keyword . '%')
+          ->orWhere('under_name_kana', 'like', '%' . $keyword . '%');
+      })
+      ->where(function ($q) use ($role, $gender) {
+        $q->whereIn('sex', $gender)
+          ->whereIn('role', $role);
+      })
+      ->whereHas('subjects', function ($q) use ($subjects) {
+        // if (!is_array($subjects)) {
+        //   $subjects = array($subjects);  // 配列でない場合、配列に変換　38行目のwhereInは、複数の条件で検索するときに使用するものだが、これを使う場合はarrayで配列に変換しなければいけない
+        // }
+        $q->whereIn('subjects.id', [1, 2, 3]);
+        // 上のif文より、[]内に配列を記述した方がシンプルだったのでこっちにした
+      })
+      ->orderBy('over_name_kana', $updown)->get();
     return $users;
   }
-
 }
